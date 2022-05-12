835f40d242e646e6e3d68b7a670a548a6e1fe562
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-05-12 23:08:44 +0300

    Add more io constructors

--- io/io.go
+++ io/io.go
@@ -4,6 +4,23 @@ type IO[A any] interface {
 	unsafeRun() (A, error)
 }
 
+type GoResult[A any] struct {
+	Value A
+	Error error
+}
+
+func (e GoResult[A])unsafeRun() (res A, err error) {
+	defer RecoverToErrorVar("GoResult.unsafeRun", &err)
+	return e.Value, e.Error
+}
+
+
+func LiftPair[A any](a A, err error) IO[A] {
+	return GoResult[A]{
+		Value: a,
+		Error: err,
+	}
+}
 
 func UnsafeRunSync[A any](io IO[A]) (A, error) {
 	return io.unsafeRun()
@@ -24,6 +41,19 @@ func (e evalImpl[A])unsafeRun() (res A, err error) {
 	defer RecoverToErrorVar("Eval.unsafeRun", &err)
 	return e.f()
 }
+
+func FromUnit(f func() error) IO[Unit] {
+	return Eval(func () (Unit, error) {
+		return Unit1, f()
+	})
+}
+
+func Pure[A any](f func() A) IO[A] {
+	return Eval(func () (A, error) {
+		return f(), nil
+	})
+}
+
 
 func MapErr[A any, B any](ioA IO[A], f func(a A)(B, error)) IO[B] {
 	return mapImpl[A, B]{
@@ -76,13 +106,18 @@ func (e flatMapImpl[A, B])unsafeRun() (res B, err error) {
 	return
 }
 
+func FlatMapErr[A any, B any](ioA IO[A], f func(a A) (B, error)) IO[B] {
+	return flatMapImpl[A, B]{
+		io: ioA,
+		f: func(a A) IO[B] { return LiftPair(f(a))},
+	}
+}
+
 func Lift[A any](a A) IO[A] {
-	return Eval(func()(A, error){return a, nil})
+	return LiftPair(a, nil)
 }
 
 func Fail[A any](err error) IO[A] {
-	return Eval(func()(a A, err1 error){
-		err1 = err
-		return 
-	})
+	var a A
+	return LiftPair(a, err)
 }
