f939010a13ae0596b2c13c79264cbd38e820aa33
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-05-22 16:11:41 +0300

    Test text writes

--- text/file_test.go
+++ text/file_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/primetalk/goio/fun"
 	"github.com/primetalk/goio/io"
 	"github.com/primetalk/goio/stream"
 	"github.com/primetalk/goio/text"
@@ -15,17 +16,33 @@ Line 2
 Line 30
 `
 
-func TestStream(t *testing.T) {
+func TestTextStream(t *testing.T) {
 	data := []byte(exampleText)
 	r := bytes.NewReader(data)
 	strings := text.ReadLines(r)
 	lens := stream.Map(strings, func(s string) int { return len(s) })
 	lensSlice, err := io.UnsafeRunSync(stream.ToSlice(lens))
-	assert.Equal(t, err, nil)
-	assert.ElementsMatch(t, lensSlice, []int{0, 6, 7})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []int{0, 6, 7}, lensSlice)
 	stream10_12 := stream.LiftMany(10, 11, 12)
 	stream20_24 := stream.Map(stream10_12, func(i int) int { return i * 2 })
 	res, err := io.UnsafeRunSync(stream.ToSlice(stream20_24))
-	assert.Equal(t, err, nil)
-	assert.Equal(t, res, []int{20, 22, 24})
+	assert.NoError(t, err)
+	assert.Equal(t, []int{20, 22, 24}, res)
+}
+
+func TestTextStreamWrite(t *testing.T) {
+	data := []byte(exampleText)
+	r := bytes.NewReader(data)
+	strings := text.ReadLines(r)
+	lens := stream.Map(strings, func(s string) int { return len(s) })
+	lensAsString := stream.Map(lens, fun.ToString[int])
+	w := bytes.NewBuffer([]byte{})
+	writes := stream.ToSink(lensAsString, text.WriteLines(w))
+	_, err := io.UnsafeRunSync(stream.DrainAll(writes))
+	assert.NoError(t, err)
+	assert.Equal(t, `0
+6
+7
+`, w.String())
 }
