0ce2ecc9520781f176827b418b1cc3c39e7c7ca2
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-05-22 17:00:34 +0300

    Test set

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func IsEven(i int) bool {
-	return i % 2 == 0
+	return i%2 == 0
 }
 
 var nats10Values = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -19,4 +19,20 @@ func TestFilter(t *testing.T) {
 
 	sumOdd := slice.Sum(slice.FilterNot(nats10Values, IsEven))
 	assert.Equal(t, 25, sumOdd)
+}
+
+func TestFlatten(t *testing.T) {
+	floatsNested := slice.Map(nats10Values, func(i int) []float32 {
+		return []float32{float32(i), float32(2 * i)}
+	})
+	floats := slice.Flatten(floatsNested)
+	assert.Equal(t, float32(55+55*2), slice.Sum(floats))
+}
+
+func TestSet(t *testing.T) {
+	intsDuplicated := slice.FlatMap(nats10Values, func(i int) []int {
+		return slice.Map(nats10Values, func(j int) int { return i + j })
+	})
+	intsSet := slice.ToSet(intsDuplicated)
+	assert.Equal(t, 19, slice.SetSize(intsSet))
 }
