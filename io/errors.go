8d74c859a2ff3336aa0a88a285bac78e73ad4c9d
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-05-31 21:18:40 +0300

    Add RecoverToLog

--- io/errors.go
+++ io/errors.go
@@ -11,7 +11,7 @@ import (
 func RecoverToErrorVar(name string, err *error) {
 	err2 := recover()
 	if err2 != nil {
-		log.Printf("RecoverToErrorVar2 (%s) (err=%+v), (err2: %+v\n", name, *err, err2)
+		log.Printf("RecoverToErrorVar(%s) (err=%+v), (err2: %+v)\n", name, *err, err2)
 		switch err2 := err2.(type) {
 		case error:
 			err4 := errors.Wrapf(err2, "%s: Recover from panic", name)
@@ -23,5 +23,13 @@ func RecoverToErrorVar(name string, err *error) {
 			err4 := fmt.Errorf("%s: Recover from unknown-panic: %+v", name, err2)
 			*err = err4
 		}
+	}
+}
+
+// RecoverToLog in case of error just logs it.
+func RecoverToLog(name string) {
+	err2 := recover()
+	if err2 != nil {
+		log.Printf("RecoverToLog(%s) (err2: %+v\n", name, err2)
 	}
 }
