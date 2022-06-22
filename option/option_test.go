1d4ef1b35def3c8ede9e56af17d906ec1551644d
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-06-22 23:43:05 +0300

    Test None

--- option/option_test.go
+++ option/option_test.go
@@ -13,9 +13,11 @@ func StringLen(s string) int {
 }
 
 var ohello = option.Some("hello")
+var onone = option.None[string]()
 
 func TestMap(t *testing.T) {
 	assert.Equal(t, option.Some(5), option.Map(ohello, StringLen))
+	assert.Equal(t, option.None[int](), option.Map(onone, StringLen))
 }
 
 func Contains(substring string) func(s string) bool {
@@ -26,9 +28,10 @@ func Contains(substring string) func(s string) bool {
 
 func TestFilter(t *testing.T) {
 	assert.Equal(t, ohello, option.Filter(ohello, Contains("llo")))
+	assert.Equal(t, onone, option.Filter(onone, Contains("llo")))
 }
 
 func TestFlatten(t *testing.T) {
 	assert.Equal(t, "hello", option.Get(option.Flatten(option.Some(ohello))))
-
+	assert.Panics(t, func() { option.Get(option.Flatten(option.Some(onone))) })
 }
