f1099572dd5e054d95ee48f54467ab09390b6347
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-08-19 00:25:42 +0300

    Add UnsafeStreamToSlice

--- stream/common_test.go
+++ stream/common_test.go
@@ -2,8 +2,11 @@ package stream_test
 
 import (
 	"fmt"
+	"testing"
 
+	"github.com/primetalk/goio/io"
 	"github.com/primetalk/goio/stream"
+	"github.com/stretchr/testify/assert"
 )
 
 var nats = stream.Unfold(0, func(s int) int {
@@ -16,3 +19,9 @@ var Mul2 = stream.MapPipe(func(i int) int { return i * 2 })
 var pipeMul2IO = stream.PipeToPairOfChannels(Mul2)
 
 var printInt = stream.NewSink(func(i int) { fmt.Printf("%d", i) })
+
+func UnsafeStreamToSlice[A any](t *testing.T, stm stream.Stream[A]) []A {
+	res, err1 := io.UnsafeRunSync(stream.ToSlice(stm))
+	assert.NoError(t, err1)
+	return res
+}
