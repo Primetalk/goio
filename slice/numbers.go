f3d475356ffe1652b313dff0ef0a2108b672251a
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-10-16 21:03:06 +0300

    feat: add Range, Nats

--- slice/numbers.go
+++ slice/numbers.go
@@ -15,3 +15,17 @@ func Sum[N Number](ns []N) (sum N) {
 	}
 	return
 }
+
+// Range starts at `from` and progresses until `to` exclusive.
+func Range(from, to int) (res []int) {
+	res = make([]int, 0, to-from)
+	for i := from; i < to; i++ {
+		res = append(res, i)
+	}
+	return
+}
+
+// Nats return slice []int{1, 2, ..., n}
+func Nats(n int) []int {
+	return Range(1, n+1)
+}
