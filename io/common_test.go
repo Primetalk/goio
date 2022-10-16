857e9b416ea7eddc7559e25c8a0776fbdf2057a8
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2022-10-16 21:02:52 +0300

    feat: add LiftFunc

--- io/common_test.go
+++ io/common_test.go
@@ -2,9 +2,11 @@ package io_test
 
 import (
 	"errors"
+	"log"
 	"testing"
 
 	"github.com/primetalk/goio/io"
+	"github.com/primetalk/goio/slice"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,6 +15,10 @@ var errorMessage = "expected error" //"on purpose failure"
 var errExpected = errors.New(errorMessage)
 
 var failure = io.Fail[string](errExpected)
+
+func inc(i int) int {
+	return i + 1
+}
 
 func UnsafeIO[A any](t *testing.T, ioa io.IO[A]) A {
 	res, err1 := io.UnsafeRunSync(ioa)
@@ -25,4 +31,13 @@ func UnsafeIOExpectError[A any](t *testing.T, expected error, ioa io.IO[A]) {
 	if assert.Error(t, err1) {
 		assert.Equal(t, expected, err1)
 	}
+}
+
+func Nats(count int) (ios []io.IO[int]) {
+	return slice.Map(slice.Range(0, count), func(i int) io.IO[int] {
+		return io.Pure(func() int {
+			log.Printf("executing %v\n", i)
+			return i
+		})
+	})
 }
